2bd3361812f172e65c64511cf26e0cb7598f33d8
Densho <denj.rey@gmail.com>
2023-10-01 23:44:17 -0400

    Initial Content for About Me and Home

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-// Define routes for your pages
+// Favicon
+app.use('/favicon.ico', express.static('images/favicon.ico'));
+
+// Routes
 app.get('/', (req, res) => {
-    res.render('home');
+    const missionStatement = "Delivering quality web solutions.";
+    res.render('home', { missionStatement });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    const fullName = "Your Full Name";
+    const aboutParagraph = "I am a dedicated and passionate web developer with a strong foundation in web technologies. My goal is to create beautiful, functional, and user-friendly websites and web applications.";
+  
+    res.render('about', { fullName, aboutParagraph });
 });
 
 app.get('/projects', (req, res) => {
@@ -29,3 +37,11 @@ app.get('/contact', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+app.post('/contact', (req, res) => {
+    const { firstName, lastName, email, message } = req.body;
+  
+    // Handle the form data (e.g., send an email, save to a database, etc.)
+  
+    res.redirect('/'); // Redirect back to the home page after form submission
+  });
