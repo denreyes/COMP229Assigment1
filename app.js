c6541fef43458f146ce53c20be24b8a114c24f9e
Densho <denj.rey@gmail.com>
2023-10-02 03:58:35 -0400

    Bootstrap setup fix

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ var usersRouter = require('./routes/users');
 var app = express();
 const port = process.env.PORT || 3000;
 
-app.use( "/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
-app.use("/js",express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
-app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")));
+// app.use( "/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
+// app.use("/js",express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
+// app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
